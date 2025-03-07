ab29f73caaa3dc8ebe3eaf709176b0906bbb5dc1
Alan Franco <alanfrancob97@gmail.com>
2025-03-07 17:44:36 -0600

    chore(keycloak): cambiar secret del cliente sadaae-api
    Se cambió el valor de la cadena secreta del cliente `sadaae-api` del servicio
    de keycloak. El valor original de la cadena secreta era una secuencia aleatoria
    de caracteres alfanuméricos y se cambió al valor `my-secret-client`. Este
    cambio se realizó para dar a entender, de manera implícita, que la cadena
    secreta del cliente es un valor que se debe de cambiar en un ambiente de
    producción.
    
    Posteriormente, se modificaron los archivos `api/default.env` y
    `api/config/config.js` para reflejar el cambio al valor de la cadena secreta
    del cliente `sadaae-api`.

--- api/config/config.js
+++ api/config/config.js
@@ -4,7 +4,7 @@ const config = {
   OPENID_ISSUER_BASE_URL: process.env.OPENID_ISSUER_BASE_URL || 'http://localhost:8080/realms/sadaae',
   OPENID_BASE_URL: process.env.OPENID_BASE_URL || 'http://localhost',
   OPENID_CLIENT_ID: process.env.OPENID_CLIENT_ID || 'sadaae-api',
-  OPENID_CLIENT_SECRET: process.env.OPENID_CLIENT_SECRET || 'hnSbkknjye4WMUcV3om0CgIgM1wSGA1Y',
+  OPENID_CLIENT_SECRET: process.env.OPENID_CLIENT_SECRET || 'my-client-secret',
   OPENID_SECRET: process.env.OPENID_SECRET || 'my-secret',
 };
 
