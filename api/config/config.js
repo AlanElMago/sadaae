00d388ae5d54b6c636d761da401c0b83dcac4fe6
Alan Franco <alanfrancob97@gmail.com>
2025-03-12 20:29:23 -0600

    chore(envs): quitar variables de entorno innecesarias
    - Se quitaron las variables de entorno relacionadas con el archivo de
    configuración `api/config/openidConfig.js` para la librería.
    `express-openid-connect` (las que comenzaban con la nomenclatura `OPENID_`).
    - Se eliminó el archivo `api/config/openidConfig.js`.

--- api/config/config.js
+++ api/config/config.js
@@ -1,11 +1,5 @@
 const config = {
   API_PORT: process.env.API_PORT || 3000,
-
-  OPENID_ISSUER_BASE_URL: process.env.OPENID_ISSUER_BASE_URL || 'http://localhost:8080/realms/sadaae',
-  OPENID_BASE_URL: process.env.OPENID_BASE_URL || 'http://localhost',
-  OPENID_CLIENT_ID: process.env.OPENID_CLIENT_ID || 'sadaae-api',
-  OPENID_CLIENT_SECRET: process.env.OPENID_CLIENT_SECRET || 'my-client-secret',
-  OPENID_SECRET: process.env.OPENID_SECRET || 'my-secret',
 };
 
 export default config;
