caa9e3bc3db03bf5a9b9009b98265303a9d8b66b
Alan Franco <alanfrancob97@gmail.com>
2025-03-12 21:00:12 -0600

    chore(envs): agregar variables de entorno OIDC
    Se agregaron las variables de entorno `OIDC_AUDIENCE`, `OIDC_ISSUER`, y
    `OIDC_JWKS_URI`, para la validación de JWTs con `express-jwt` y `jwks_rsa`.

--- api/config/config.js
+++ api/config/config.js
@@ -1,5 +1,9 @@
 const config = {
   API_PORT: process.env.API_PORT || 3000,
+
+  OIDC_AUDIENCE: process.env.OIDC_AUDIENCE || 'http://localhost:3000/api',
+  OIDC_ISSUER: process.env.OIDC_ISSUER || 'http://localhost:8080/realms/sadaae',
+  OIDC_JWKS_URI: process.env.OIDC_JWKS_URI || 'http://localhost:8080/realms/sadaae/protocol/openid-connect/certs',
 };
 
 export default config;
