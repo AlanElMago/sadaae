db50a12490316c9d09515acf64a6e949adb37583
Alan Franco <alanfrancob97@gmail.com>
2025-03-13 11:44:48 -0600

    test(auth): agregar rutas de prueba para validar roles
    Se agregaron rutas para probar la validacion de roles dentro del API:
    - `GET /api/test/auth/customer`
    - `GET /api/test/auth/camera`
    - `GET /api/test/auth/operator`
    - `GET /api/test/auth/admin`

--- api/test/routes/authTest.routes.js
+++ api/test/routes/authTest.routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
 import authTestController from '../controllers/authTest.controller.js';
+import requireAuth from '../../middleware/requireAuth.js';
 
 const router = express.Router();
 
 router.get('/protect', authTestController.protect);
+router.get('/customer', requireAuth(['customer']), authTestController.customer);
+router.get('/camera', requireAuth(['camera']), authTestController.camera);
+router.get('/operator', requireAuth(['operator']), authTestController.operator);
+router.get('/admin', requireAuth(['admin']), authTestController.admin);
 
 export default router;
