2aee9e8bdda8aacb537f816406c526cd6bcfa7f7
Alan Franco <alanfrancob97@gmail.com>
2025-03-12 21:09:40 -0600

    update(server.js): reemplazar express-openid-connect
    Se reemplazó `express-openid-connect` por `express-jwt` en el archivo
    `api/server.js`.

--- api/server.js
+++ api/server.js
@@ -1,27 +1,20 @@
 import express from 'express';
-import { auth } from 'express-openid-connect';
 import http from 'http2';
 
 import config from './config/config.js';
-import openidConfig from './config/openidConfig.js';
+import requireAuth from './middleware/requireAuth.js';
+import validateJwt from './middleware/validateJwt.js';
 import authTestRouts from './test/routes/authTest.routes.js';
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(auth(openidConfig));
-app.use((err, req, res, next) => {
-  if (err.status === http.constants.HTTP_STATUS_UNAUTHORIZED) {
-    return res.status(http.constants.HTTP_STATUS_UNAUTHORIZED).json({ message: 'No autorizado!' });
-  }
-
-  next(err);
-})
+app.use(validateJwt);
 
 // rutas
 app.get('/api', (req, res) => res.status(http.constants.HTTP_STATUS_OK).json({ message: 'Hola, Mundo!' }));
-app.use('/api/test/auth', authTestRouts);
+app.use('/api/test/auth', requireAuth, authTestRouts);
 
 // iniciar el servidor
 app.listen(config.API_PORT);
