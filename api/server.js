18968bddc4e733bcde9879f589aa6c93809bfb61
Alan Franco <alanfrancob97@gmail.com>
2025-03-14 12:52:18 -0600

    fix(server.js): corregir typo
    Se corrigió un typo en la importación de rutas de prueba de autenticación.

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ import http from 'http2';
 import config from './config/config.js';
 import requireAuth from './middleware/requireAuth.js';
 import validateJwt from './middleware/validateJwt.js';
-import authTestRouts from './test/routes/authTest.routes.js';
+import authTestRoutes from './test/routes/authTest.routes.js';
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(validateJwt);
 
 // rutas
 app.get('/api', (req, res) => res.status(http.constants.HTTP_STATUS_OK).json({ message: 'Hola, Mundo!' }));
-app.use('/api/test/auth', requireAuth(), authTestRouts);
+app.use('/api/test/auth', requireAuth(), authTestRoutes);
 
 // iniciar el servidor
 app.listen(config.API_PORT, () => console.log(`Servidor escuchando en el puerto ${config.API_PORT}`));
