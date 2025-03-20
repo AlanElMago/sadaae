f071aa60fa32509cb9367bc7ca4bb5518d57c110
Alan Franco <alanfrancob97@gmail.com>
2025-03-20 14:29:00 -0600

    chore(envs): agregar variable de entorno OIDC_TOKEN_URI
    Se agregó la variable de entorno `OIDC_TOKEN_URI` al archivo `api/default.env`
    y `api/config/config.js` para las consultas de autorización UMA

--- api/config/config.js
+++ api/config/config.js
@@ -4,6 +4,7 @@ const config = {
   OIDC_AUDIENCE: process.env.OIDC_AUDIENCE || 'http://localhost:3000/api',
   OIDC_ISSUER: process.env.OIDC_ISSUER || 'http://localhost:8080/realms/sadaae',
   OIDC_JWKS_URI: process.env.OIDC_JWKS_URI || 'http://localhost:8080/realms/sadaae/protocol/openid-connect/certs',
+  OIDC_TOKEN_URI: process.env.OIDC_TOKEN_URI || 'http://localhost:8080/realms/sadaae/protocol/openid-connect/token',
 };
 
 export default config;
