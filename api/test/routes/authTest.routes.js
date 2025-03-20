2d2085d46843438d318e529aa3a3904907e033d6
Alan Franco <alanfrancob97@gmail.com>
2025-03-20 14:33:44 -0600

    refactor(auth): modificar requireAuth para autorización UMA
    Se modificó el middleware `requireAuth` para que verifique si el usuario tiene
    permisos para acceder a un recurso a través del API de autorización UMA de
    Keycloak. Se modificó el parámetro de `requireAuth` para que reciba una cadena
    que describa el alcance que se requiere para acceder al recurso (e.j. `create`,
    `read`, `update`, `delete`, etc.).

--- api/test/routes/authTest.routes.js
+++ api/test/routes/authTest.routes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 
 import authTestController from '../controllers/authTest.controller.js';
-import requireAuth from '../../middleware/requireAuth.js';
 
 const router = express.Router();
 
 router.get('/protect', authTestController.protect);
-router.get('/customer', requireAuth(['customer']), authTestController.customer);
-router.get('/camera', requireAuth(['camera']), authTestController.camera);
-router.get('/operator', requireAuth(['operator']), authTestController.operator);
-router.get('/admin', requireAuth(['admin']), authTestController.admin);
+router.get('/customer', authTestController.customer);
+router.get('/camera', authTestController.camera);
+router.get('/operator', authTestController.operator);
+router.get('/admin', authTestController.admin);
 
 export default router;
