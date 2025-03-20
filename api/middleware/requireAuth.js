314997ae10bab4ff664fce479ad66daf06fa7944
Alan Franco <alanfrancob97@gmail.com>
2025-03-20 14:33:44 -0600

    refactor(auth): modificar requireAuth para autorización UMA
    Se modificó el middleware `requireAuth` para que verifique si el usuario tiene
    permisos para acceder a un recurso a través del API de autorización UMA de
    Keycloak. Se modificó el parámetro de `requireAuth` para que reciba una cadena
    que describa el alcance que se requiere para acceder al recurso (e.j. `create`,
    `read`, `update`, `delete`, etc.).

--- api/middleware/requireAuth.js
+++ api/middleware/requireAuth.js
@@ -1,39 +1,72 @@
+import axios from 'axios';
 import http from 'http2';
 
+import config from '../config/config.js';
+
 /**
- * Middleware que valida si el usuario tiene un token de acceso y si tiene los permisos necesarios (rol) para acceder a
- * un recurso. Un usuario con rol de administrador (`admin`) tiene acceso a todos los recursos del API.
- * @param {[string]} authorizedRoles - Arreglo de roles permitidos para acceder al recurso. Si el arreglo está vacío,
- * se permite el acceso a cualquier usuario con token de acceso.
+ * Middleware que valida si el usuario tiene un token de acceso y si tiene los permisos necesarios para acceder a un
+ * recurso.
+ * @param {string} scope - Alcance que se requiere para acceder al recurso (e.j. `'create'` `'read'`, `'update'`,
+ * `'delete'`). Si no se especifica, implica que el recurso no requiere de un alcance en específico.
  * @returns {function} Middleware de Express que valida si el usuario tiene un token de acceso y si tiene los permisos
- * necesarios (rol) para acceder a un recurso.
+ * necesarios para acceder a un recurso.
+ * 
+ * @example
+ * import express from 'express';
+ * import requireAuth from '../middleware/requireAuth.js';
+ * 
+ * const router = express.Router();
+ * 
+ * router.get('/user/:id', requireAuth('read'), (req, res) => {
+ *   res.status(200).json({ message: 'Usuario encontrado' });
+ * }
+ * 
+ * router.get('/protected', requireAuth(), (req, res) => {
+ *   res.status(200).json({ message: 'Recurso protegido' });
+ * })
  */
-const requireAuth = (authorizedRoles = []) => {
-  return (req, res, next) => {
+const requireAuth = (scope = "") => {
+  return async (req, res, next) => {
     if (!req.auth) {
       return res
         .status(http.constants.HTTP_STATUS_UNAUTHORIZED)
         .json({ message: "No se encontró el token de acceso" });
     }
 
-    if (authorizedRoles.length <= 0) {
-      return next();
+    const formData = new FormData();
+    const permissionUri = req.originalUrl.replace('/api', '');
+
+    formData.append('grant_type', 'urn:ietf:params:oauth:grant-type:uma-ticket');
+    formData.append('response_mode', 'decision');
+    formData.append('audience', 'sadaae-api');
+    formData.append('permission', `${permissionUri}#${scope}`);
+    formData.append('permission_resource_format', 'uri');
+    formData.append('permission_resource_matching_uri', 'true');
+
+    try {
+      const authResponse = await axios({
+        method: 'post',
+        url: config.OIDC_TOKEN_URI,
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': req.headers.authorization,
+        },
+        validateStatus: (status) => (status >= 200 && status < 500),
+      });
+
+      if (authResponse.data.result === true) {
+        return next();
+      }
+
+      return res.status(authResponse.status).json(authResponse.data);
     }
+    catch (error) {
+      console.log(error);
 
-    const userRoles = req.auth.realm_access.roles;
-
-    if (userRoles.includes('admin')) {
-      return next();
+      return res.status(http.constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({ message: error });
     }
-
-    if (userRoles.some(userRole => authorizedRoles.includes(userRole))) {
-      return next();
-    }
-
-    return res
-      .status(http.constants.HTTP_STATUS_UNAUTHORIZED)
-      .json({ message: "No tienes permisos para acceder a este recurso" });
-  };
+  }
 };
 
 export default requireAuth;
