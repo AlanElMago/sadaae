34ad9a9119e0153d1a248d50d9b4adcb32a48f51
Alan Franco <alanfrancob97@gmail.com>
2025-03-24 12:56:12 -0600

    feat(api): agregar cliente de Keycloak y rutas para cliente
    Se modificó el archivo `api/server.js` para agregar el cliente de Keycloak y
    las rutas para el cliente (`/api/v1/customer`).

--- api/server.js
+++ api/server.js
@@ -2,14 +2,19 @@ import express from 'express';
 import http from 'http2';
 import mongoose from 'mongoose';
 
+import keycloakClient from './keycloakClient.js';
 import config from './config/config.js';
 import mongooseConnectOptions from './config/mongooseConnectOptions.js';
 import requireAuth from './middleware/requireAuth.js';
 import validateJwt from './middleware/validateJwt.js';
 import authTestRoutes from './test/routes/authTest.routes.js';
+import customerRoutes from './v1/routes/customer.routes.js';
 import establishmentRoutes from './v1/routes/establishment.routes.js';
 
 await mongoose.connect(config.MONGO_URI, mongooseConnectOptions);
+await keycloakClient.connect(config.KC_REALM_URL, config.KC_CLIENT_ID, config.KC_CLIENT_SECRET, () => {
+  console.log('Conexión a Keycloak exitosa');
+});
 
 const app = express();
 
@@ -20,6 +25,7 @@ app.use(validateJwt);
 // rutas
 app.get('/api', (req, res) => res.status(http.constants.HTTP_STATUS_OK).json({ message: 'Hola, Mundo!' }));
 app.use('/api/v1/establishments', establishmentRoutes);
+app.use('/api/v1/customer', customerRoutes);
 app.use('/api/test/auth', requireAuth(), authTestRoutes);
 
 // iniciar el servidor
