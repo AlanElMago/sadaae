35e1f732ad89d071978aada2e06730841cb612d5
Alan Franco <alanfrancob97@gmail.com>
2025-03-25 22:16:51 -0600

    feat(api): agregar modelo de cámara
    Se agregó el archivo `api/v1/models/camera.model.js` que contiene la
    definición de un modelo de Mongoose que representa una cámara de seguridad
    dentro de un negocio.

---
+++ api/v1/models/camera.model.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+
+const cameraApiKeySchema = new mongoose.Schema({
+  hash: {
+    type: String,
+    required: true,
+  },
+  rotatedAt: {
+    type: Date,
+    required: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+const cameraSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    requred: false,
+  },
+  serialNumber: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+    required: true,
+  },
+  apiKey: {
+    type: cameraApiKeySchema,
+    required: true,
+  },
+  enabled: {
+    type: Boolean,
+    default: false,
+  },
+  userId: {
+    type: String,
+    alias: 'ownerId',
+    default: null,
+  },
+  establishmentId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'Establishment',
+    default: null,
+  },
+  activationCode: {
+    type: String,
+    required: true,
+    immutable: true,
+  },
+  activatedBy: {
+    type: String,
+    default: null,
+  },
+  createdBy: {
+    type: String,
+    required: true,
+    immutable: true,
+  },
+  updatedBy: {
+    type: String,
+    required: true,
+  },
+},
+{
+  timestamps: {
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
+  },
+});
+
+export const Camera = mongoose.model('Camera', cameraSchema);
+export const CameraApiKey = mongoose.model('Camera').discriminator('CameraApiKeyClass', cameraApiKeySchema);
