3d6a0864623f20c0cc76660594ea6ac63a80108f
Alan Franco <alanfrancob97@gmail.com>
2025-03-27 11:28:34 -0600

    rearrange(api): renombrar controlador cliente
    - Se renombró el archivo `api/v1/controllers/customer.controller.js` a
    `api/v1/controllers/customerEstablishment.controller.js` para reflejar mejor la
    funcionalidad del controlador, que ahora maneja sólamente los establecimientos
    de los clientes.
    - Se actualizó la importación correspondiente en
    `api/v1/routes/customer.routes.js` para asegurar la correcta referencia al
    nuevo nombre del controlador.

--- api/v1/routes/customer.routes.js
+++ api/v1/routes/customer.routes.js
@@ -1,15 +1,15 @@
 
 import express from 'express';
 
-import customerController from '../controllers/customer.controller.js';
+import customerEstablishmentController from '../controllers/customerEstablishment.controller.js';
 import requireAuth from '../../middleware/requireAuth.js';
 
 const router = express.Router();
 
-router.get('/:ownerId/establishments', requireAuth('read'), customerController.get);
-router.get('/:ownerId/establishments/:id', requireAuth('read'), customerController.getById);
-router.post('/:ownerId/establishments', requireAuth('create'), customerController.create);
-router.patch('/:ownerId/establishments/:id', requireAuth('update'), customerController.update);
-router.delete('/:ownerId/establishments/:id', requireAuth('delete'), customerController.delete);
+router.get('/:ownerId/establishments', requireAuth('read'), customerEstablishmentController.get);
+router.get('/:ownerId/establishments/:id', requireAuth('read'), customerEstablishmentController.getById);
+router.post('/:ownerId/establishments', requireAuth('create'), customerEstablishmentController.create);
+router.patch('/:ownerId/establishments/:id', requireAuth('update'), customerEstablishmentController.update);
+router.delete('/:ownerId/establishments/:id', requireAuth('delete'), customerEstablishmentController.delete);
 
 export default router;
