21b420a914c618044880ee8bbbb0ca22e743e1de
Alan Franco <alanfrancob97@gmail.com>
2025-03-31 16:56:11 -0600

    feat(service): registrar recurso UMA tipo cámara
    Se agregó la función `registerCamera` al servicio de Keycloak
    (`api/v1/services/keycloak.service.js`) para registrar recursos UMA de tipo
    "cámara" a Keycloak.

--- api/v1/services/keycloak.service.js
+++ api/v1/services/keycloak.service.js
@@ -34,4 +34,40 @@ const registerEstablishment = async (originalUrl, establishmentId, ownerId, owne
   return createResourcePermissionResponse;
 };
 
-export default { registerEstablishment };
+/**
+ * Registra un recurso UMA de tipo "Cámara" en Keycloak
+ * @param {*} originalUrl - URL original de la solicitud
+ * @param {*} cameraId - ID de la cámara a registrar
+ * @param {*} ownerId - ID del propietario de la cámara
+ * @param {*} ownerBearerToken - Bearer token del propietario de la cámara
+ * @returns {Promise<import('axios').AxiosResponse>} Promesa con el resultado de la operación
+ */
+const registerCamera = async (originalUrl, cameraId, ownerId, ownerBearerToken) => {
+  const resourceName = `camera-${cameraId}`;
+  const resourceUri = `${originalUrl.replace('/api', '')}/${cameraId}`;
+  const createUmaResorceResponse = await keycloakClient.createUmaResource(resourceName, {
+    displayName: resourceName,
+    type: `${config.API_BASE_URL}${originalUrl.replace('/api', '')}`,
+    owner: `${ownerId}`,
+    ownerManagedAccess: true,
+    uris: [
+      resourceUri,
+      `${resourceUri}/assign-establishment`,
+    ],
+    resource_scopes: ['read', 'update'],
+  });
+
+  const permissionName = `camera-permission-${cameraId}`;
+  const createResourcePermissionResponse = await keycloakClient.createResourcePermission(
+    permissionName,
+    createUmaResorceResponse.data._id,
+    ownerBearerToken, {
+      description: `Permiso que aplica al cliente ${ownerId} para acceder a la cámara ${cameraId}`,
+      scopes: ['read', 'update'],
+      users: [`${ownerId}`],
+  });
+
+  return createResourcePermissionResponse;
+}
+
+export default { registerEstablishment, registerCamera };
