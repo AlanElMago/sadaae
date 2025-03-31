92e0aefaaa51017c355698faea23b2e19f921502
Alan Franco <alanfrancob97@gmail.com>
2025-03-31 17:13:48 -0600

    feat(route): agregar rutas de cámaras para clientes
    Se agregaron rutas adicionales al archivo `api/v1/routes/customer.routes.js`
    que están relacionadas con la gestión de las cámaras de los clientes:
    
    - `GET /api/v1/customer/:ownerId/cameras` - obtiene un rango de cámaras de un
    cliente.
    - `GET /api/v1/customer/:ownerId/cameras/:id` - obtiene una cámara específica
    de un cliente.
    - `POST /api/v1/customer/:ownerId/cameras` - activa una cámara usando su número
    serial y código de activación. La cámara se registra bajo el `id` del cliente
    que la registró.
    - `PATCH /api/v1/customer/:ownerId/cameras/:id` - actualiza la informacion de
    una cámara específica de un cliente.
    - `PATCH /api/v1/customer/:ownerId/cameras/:id/assign-establishment`: Asigna
    una cámara específica a un establecimiento de un cliente.

--- api/v1/routes/customer.routes.js
+++ api/v1/routes/customer.routes.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 
+import customerCameraController from '../controllers/customerCamera.controller.js';
 import customerEstablishmentController from '../controllers/customerEstablishment.controller.js';
 import requireAuth from '../../middleware/requireAuth.js';
 
@@ -11,5 +12,11 @@ router.get('/:ownerId/establishments/:id', requireAuth('read'), customerEstablis
 router.post('/:ownerId/establishments', requireAuth('create'), customerEstablishmentController.create);
 router.patch('/:ownerId/establishments/:id', requireAuth('update'), customerEstablishmentController.update);
 router.delete('/:ownerId/establishments/:id', requireAuth('delete'), customerEstablishmentController.delete);
+
+router.get('/:ownerId/cameras', requireAuth('read'), customerCameraController.get);
+router.get('/:ownerId/cameras/:id', requireAuth('read'), customerCameraController.getById);
+router.post('/:ownerId/cameras', requireAuth('activate'), customerCameraController.activate);
+router.patch('/:ownerId/cameras/:id', requireAuth('update'), customerCameraController.update);
+router.patch('/:ownerId/cameras/:id/assign-establishment', requireAuth('update'), customerCameraController.assignEstablishment);
 
 export default router;
