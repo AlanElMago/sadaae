788420ed4a4731e09d19f29d712fbd767423afb2
Alan Franco <alanfrancob97@gmail.com>
2025-04-21 14:41:14 -0600

    test(python): probar auth del servicio de notificaciones
    Se modificaron los scripts `python/recieve_messages.py` y
    `python/send_messages.py` para probar la autentiación del servicio de
    notificaciones por medio de JWT.

--- python/send_messages.py
+++ python/send_messages.py
@@ -3,8 +3,14 @@ import concurrent.futures
 import socketio
 
 from typing import Final
+from oauthlib.oauth2.rfc6749.errors import InvalidGrantError
+import socketio.exceptions
+
+from sadaae import login
 
 NOTIFICATIONS_URL: Final = "http://localhost:3001"
+ADMIN_USER_EMAIL: Final = "admin@example.com"
+ADMIN_USER_PASSWORD: Final = "admin1234%"
 
 sio = socketio.AsyncClient()
 
@@ -16,17 +22,38 @@ async def connect() -> None:
 async def disconnect() -> None:
     print("Desconnectado del servidor de notificaciones")
 
+@sio.event
+async def error(error_message) -> None:
+    print(f"Error: {error_message}")
+
 async def main() -> None:
-    await sio.connect(NOTIFICATIONS_URL)
+    user_email = input("Email (admin@example.com): ")
+    user_password = input("Password (admin1234%): ")
+
+    user_email = user_email if user_email else ADMIN_USER_EMAIL
+    user_password = user_password if user_password else ADMIN_USER_PASSWORD
+
+    try:
+        oauth = login(user_email, user_password)
+    except InvalidGrantError:
+        print("Credenciales inválidas. Verifica su correo y contraseña.")
+        return
+
+    auth = {"token": oauth.token['access_token']}
+
+    try:
+        await sio.connect(NOTIFICATIONS_URL, auth=auth)
+    except socketio.exceptions.ConnectionError:
+        return
 
     prompt = "Enviar mensaje ('salir' para salir): "
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
 
-    while True:
+    while sio.connected:
         user_input = await asyncio \
             .get_event_loop() \
             .run_in_executor(executor, input, prompt)
-
+        
         if user_input == "salir":
             break
 
