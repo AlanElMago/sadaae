a0e94245b7eba62ad2fd072a9b3ed248b18df2f6
Alan Franco <alanfrancob97@gmail.com>
2025-04-21 14:27:13 -0600

    chore(envs): agregar variables de entorno OIDC
    Se agregaron las variables de entorno `OIDC_AUDIENCE`, `OIDC_ISSUER`, y
    `OIDC_JWKS_URI` a los archivos `notifications/config/config.js` y
    `notifications/default.env` para configurar la verificación de JWT en el
    servicio de notificaciones.

--- notifications/config/config.js
+++ notifications/config/config.js
@@ -1,5 +1,9 @@
 const config = {
-  NOTIFICATIONS_PORT: process.env.NOTIFICATIONS_PORT || '3001',
+  NOTIFICATIONS_PORT: process.env.NOTIFICATIONS_PORT || 3001,
+
+  OIDC_AUDIENCE: process.env.OIDC_AUDIENCE || 'http://localhost:3000/api',
+  OIDC_ISSUER: process.env.OIDC_ISSUER || 'http://localhost:8080/realms/sadaae',
+  OIDC_JWKS_URI: process.env.OIDC_JWKS_URI || 'http://localhost:8080/realms/sadaae/protocol/openid-connect/certs',
 };
 
 export default config;
